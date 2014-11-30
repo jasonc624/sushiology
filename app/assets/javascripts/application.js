0e515bd3fa0d307823aa2a0473b3f06fc95a3ec5
jasonc624 <jasonc62492@gmail.com>
2014-11-30 01:35:27 -0500

    home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require cart
+//= require script
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-root 'home#index'
+get 'home', to: 'application#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'application#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
